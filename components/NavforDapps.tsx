d99e88f29fccb017946036bdf36d91db6ad17c10
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2024-08-26 00:31:23 +0530

    Dapps navbar all links working

---
+++ components/NavforDapps.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const menus = [
+  {
+    title: "Swap",
+    href: "/Dapps",
+  },
+  {
+    title: "DE-Fi",
+    href: "/Dapps/DE-Fi",
+  },
+  {
+    title: "Game-Fi",
+    href: "/Dapps/Game-Fi",
+  },
+  {
+    title: "Socials",
+    href: "/Dapps/Socials",
+  },
+  {
+    title: "Marketplace",
+    href: "/Dapps/Marketplace",
+  },
+];
+
+const NavforDapps = () => {
+  const pathName = usePathname();
+
+  return (
+    <div className="px-4 space-y-4">
+      <div className="flex space-x-4 mb-4">
+        {menus.map((menu) => (
+          <Link
+            key={menu.title}
+            href={menu.href}
+            className={`${
+              pathName === menu.href
+                ? "text-white border-b-2 border-red-500 pb-1"
+                : "text-gray-400"
+            }`}
+          >
+            <button>{menu.title}</button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NavforDapps;
