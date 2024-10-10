0f6ca09a619cf193e61d2c34175e5461b34c595d
rahulkathuriaaa <rahulkathuria008@gmail.com>
2024-10-10 11:36:52 +0530

    debugging main balance pt2

--- components/test.tsx
+++ components/test.tsx
@@ -365,8 +365,6 @@ const WalletScreen = () => {
 
         if (tokenSymbol === 'APT' && aptPrice !== null) {
           newTotalBalance += formattedTokenBalance * aptPrice;
-        } else {
-          newTotalBalance += formattedTokenBalance;
         }
       }
   
@@ -601,7 +599,7 @@ const WalletScreen = () => {
             <span className="text-xl text-green-400">Main Balance</span>
             <h2 className="text-4xl mt-1 font-semibold"> $
             {/* {isBalanceVisible ? (totalBalance * (price || 0)).toFixed(3) : '*****'} */}
-            ${isBalanceVisible ? totalBalance.toFixed(3) : '*****'}
+            {isBalanceVisible ? totalBalance.toFixed(3) : '*****'}
 
             </h2>
           </div>
